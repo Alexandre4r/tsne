8fb4df33ff6f04b0b491551ef2aca6b9a605f032
Alexandre4r <alexandre.balcon21@gmail.com>
2025-06-09 17:51:06 +0200

    Update node.py

--- bh/node.py
+++ bh/node.py
@@ -1,26 +1,18 @@
-from config import MIN_NODE_SIZE
+from config import EPS
 import numpy as np
+
 
 class Node:
 
     def __init__(self, x, y, L):
         self.com = np.array([x + L / 2, y + L / 2], dtype=float)  # centre de masse
-        self.b_array = None  # body dans la cellule
+        self.body = None  # body dans la cellule
+        self.isLeaf = True
         self.x = x
         self.y = y
         self.L = L  # taille
         self.child = None  # tableau arbres fils
         self.nb_body = 0
-
-    def __str__(self):
-        if self.nb_body == 0:
-            return ""
-        if not (self.b_array is None):
-            return f"{[body.__str__() for body in self.b_array]}"
-        res = ""
-        for c in self.child:
-            res += f"{c.__str__()},"
-        return "{" + res[:-1] + "}"
 
     def subdivide(self):
         """
@@ -33,10 +25,15 @@ class Node:
             Node(self.x, self.y + new_L, new_L),
             Node(self.x + new_L, self.y + new_L, new_L)
         ]
+        self.isLeaf = False
         return
 
+    def _is_duplicate(self, point1: np.array, point2: np.array):
+
+        return abs(point1[0] - point2[0]) < EPS and abs(point1[1] - point2[1]) < EPS
+
     def find(self, body):
-        t = (self.L) / 2
+        t = self.L / 2
         if body[1] > self.y + t:
             if body[0] > self.x + t:
                 return 3
@@ -48,21 +45,20 @@ class Node:
             else:
                 return 0
 
-    def insert(self, body):
+    def insert(self, body, i):
 
         # Cas arbre vide
         if self.nb_body == 0:
-            self.b_array = [body]
+            self.body = body
 
         # cas limite(taille de l'arbre)
-        elif self.L < MIN_NODE_SIZE:
-            self.b_array.append(body)
-
+        elif self._is_duplicate(self.com, body):
+            pass
         else:
-            if self.b_array != None:
+            if self.body is not None:
                 self.subdivide()
-                self.child[self.find(self.b_array[0])].insert(self.b_array[0])
-                self.b_array = None
+                self.child[self.find(self.body)].insert(self.body)
+                self.body = None
             self.child[self.find(body)].insert(body)
 
         self.nb_body += 1
@@ -70,10 +66,8 @@ class Node:
 
     def center_of_mass(self):
 
-        if self.b_array is not None:
-            for body in self.b_array:
-                self.com = (self.com[0] + body[0], self.com[1] + body[1])
-            self.com = (self.com[0] / self.nb_body, self.com[1] / self.nb_body)
+        if self.body is not None:
+            self.com = self.body
 
         elif self.child is not None:
             for child in self.child:
